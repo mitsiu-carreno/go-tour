8262976632bade5560efb859c313eb7c7964581c
mitsiu <mitsiu.carreno@gmail.com>
2018-02-15 02:00:50 -0600

    add basic package ToDo: test package

---
+++ tour.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mitsiu-carreno/go-tour/stringutil"
+)
+
+func main() {
+	fmt.Println("This is where i'll put all my knowladge from the go tour")
+	fmt.Println(stringutil.Reverse("evol ym yraK"))
+}
